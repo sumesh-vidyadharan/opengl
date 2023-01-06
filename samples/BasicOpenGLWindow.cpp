caf20049cc3bb3f2b9093e0314c0fdf820b39cd7
Vidyadharan Sumesh (Consultant) <sumesh.vidyadharan@consultant.volvo.com>
2023-01-06 17:38:24 +0100

    Corrected the folder structure. Created basic window

--- samples/BasicOpenGLWindow.cpp
+++ samples/BasicOpenGLWindow.cpp
@@ -1,6 +1,65 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
 #include <iostream>
 
-int main(){
-    std::cout << "Test" << std::endl;
+const unsigned int screen_width = 800;
+const unsigned int screen_height = 600;
+
+void error_callback(int error, const char *description)
+{
+    std::cerr << "Error: " << description << std::endl;
+}
+
+// Whenever the window size changed this callback function executes
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    glfwSetErrorCallback(error_callback);
+
+    // Initialize and configure glfw
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // OpenGL version 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Create window using glfw
+    GLFWwindow *window = glfwCreateWindow(screen_width, screen_height, "OpenGL", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return 1;
+    }
+
+    // Render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Set color for the window
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        // Poll IO events
+        glfwPollEvents();
+    }
+
+    // Delete the created window
+    glfwDestroyWindow(window);
+    // Terminate glfw
+    glfwTerminate();
     return 0;
 }
